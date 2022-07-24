a4de30b4909a90c5bc86b98ceccf6a2c7ecdf837
Salman H <Salman0112358@outlook.com>
2022-07-24 17:07:31 +0100

    adding react button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { greet } from "./utils/greet";
+import Button from './Components/Button'
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  return (
+    <Button/>
+  )
 }
-
 export default App;
---
+++ src/Components/Button.tsx
@@ -0,0 +1,9 @@
+export default function Button ():JSX.Element{
+
+    return (
+        <>
+        <h3>Press the button below to play</h3>
+        <button className="main-btn">FizzBuzz</button>
+        </>
+    )
+}
