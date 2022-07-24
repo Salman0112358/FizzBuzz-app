23488850b2eb61a24f9b5118190c1e164ae2279b
Salman H <Salman0112358@outlook.com>
2022-07-24 17:49:09 +0100

    working fizzbuzz using 2 use states

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import Button from './Components/Button'
 
 function App(): JSX.Element {
   return (
+    <>
     <Button/>
+    </>
+ 
   )
 }
 export default App;
--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,9 +1,38 @@
+import {useState} from 'react'
+let Buttoncounter = 0
+
 export default function Button ():JSX.Element{
+
+    
+    const [currentVal, nextVal] = useState<(number|string)>('')
+    const [currentArray, nextArray] = useState<(number|string)[]>([])
+
+    const result = () => {
+
+        Buttoncounter += 1
+        console.log(Buttoncounter)
+  
+        if (Buttoncounter % 3 === 0 && Buttoncounter % 5 === 0){ 
+            nextVal('FizzBuzz -')
+            nextArray([...currentArray,currentVal])     
+        } else if (Buttoncounter % 3 === 0) {
+            nextVal('Fizz -')
+            nextArray([...currentArray,currentVal]) 
+
+        } else if (Buttoncounter % 5 ===0) {
+            nextVal('Buzz -')
+            nextArray([...currentArray,currentVal]) 
+        } else {
+            nextVal(`${Buttoncounter} -`)
+            nextArray([...currentArray,currentVal]) 
+        } 
+    }
 
     return (
         <>
         <h3>Press the button below to play</h3>
-        <button className="main-btn">FizzBuzz</button>
+        <button className="main-btn" onClick={result}>FizzBuzz</button>
+        <p>{currentArray}</p>
         </>
     )
 }
